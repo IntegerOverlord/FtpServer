979c7867a9ca1862e1b9f2d92b3210efe5c1851a
Maksym Fedoriaka <maksym.f@it-dimension.com>
2019-06-08 17:26:18 +0300

    Added UTF8 support, text file transfer works, regular files WIP

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -8,9 +8,13 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdlib.h>
+#include <io.h>
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <fcntl.h>
+#include <direct.h>
+#include <thread>
 
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
@@ -138,6 +142,343 @@ std::string Trim(std::string in)
 	return in;
 }
 
+void working_thread(SOCKET command_socket)
+{
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+	int iResult;
+
+	std::string to_send;
+
+	bool image_mode = false;
+	SOCKET data_socket = INVALID_SOCKET;
+	File currentWorkingDirectory = File::getCurrentWorkingDirectory();
+
+	to_send = "220 Service ready for new user.\n";
+	if (send(command_socket, to_send.c_str(), to_send.length(), 0) == -1)
+	{
+		std::cout << "Error sending." << std::endl;
+	}
+
+	// Receive until the peer shuts down the connection
+	do {
+
+		iResult = recv(command_socket, recvbuf, recvbuflen, 0);
+		if (iResult > 0) {
+			std::string msg = "";
+			for (int x = 0; x < iResult; x++)
+			{
+				msg.push_back(recvbuf[x]);
+			}
+
+			std::cout << "Received: " << msg << std::endl;
+
+			if (msg.substr(0, 4) == "USER")
+			{
+				// Accept all users because I'm secure like that
+				to_send = "230 User logged in, proceed.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 4) == "PASS")
+			{
+				// Accept all users because I'm secure like that
+				to_send = "230 User logged in, proceed.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 4) == "NOOP")
+			{
+				// No operation
+				to_send = "200 Command okay.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 4) == "QUIT")
+			{
+				// Quit
+				to_send = "221 Service closing control connection.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 3) == "PWD")
+			{
+				to_send = "257 \"" + currentWorkingDirectory.getFullPathName().toStdString() + "\\\" is your current location\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 4) == "TYPE")
+			{
+				if (msg.substr(5, 1) == "I")
+				{
+					image_mode = true;
+				}
+				else
+				{
+					image_mode = false;
+				}
+
+				to_send = "200 Command okay.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 4) == "PORT")
+			{
+				// open data port
+				int c2 = msg.rfind(",");
+				int c1 = msg.rfind(",", c2 - 1);
+
+				// Calculate port number
+				int portNumber = atoi(msg.substr(c1 + 1, c2 - c1).c_str()) * 256;
+				portNumber += atoi(msg.substr(c2 + 1).c_str());
+
+				// Get address
+				std::string address = msg.substr(5, c1 - 5);
+				for (int i = 0; i < address.size(); i++)
+				{
+					if (address.substr(i, 1) == ",")
+					{
+						address.replace(i, 1, ".");
+					}
+				}
+
+				// Get the file descriptor
+				std::cout << "Attempting to open port: " << portNumber << std::endl;
+				std::cout << "From address: " << address << std::endl;
+				data_socket = getSocketFD(portNumber, false, address);
+
+				if (data_socket == INVALID_SOCKET)
+				{
+					// Port open failure
+					to_send = "425 Can't open data connection.\n";
+					std::cout << "Answered: " << to_send << std::endl;
+					send(command_socket, to_send.c_str(), to_send.size(), 0);
+					continue;
+				}
+
+				// Port open success
+				to_send = "200 Command okay.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 4) == "LIST")
+			{
+				// First, make sure you're in image mode
+				if (!image_mode)
+				{
+					// Not yet in image mode
+					to_send = "451 Requested action aborted: local error in processing\n";
+					std::cout << "Answered: " << to_send << std::endl;
+					send(command_socket, to_send.c_str(), to_send.size(), 0);
+					continue;
+				}
+
+				// Now, make sure PORT has been called
+				if (data_socket == INVALID_SOCKET)
+				{
+					// Port hasn't been set
+					to_send = "451 Requested action aborted: local error in processing\n";
+					std::cout << "Answered: " << to_send << std::endl;
+					send(command_socket, to_send.c_str(), to_send.size(), 0);
+					continue;
+				}
+
+				// Open directory
+				File& directory = currentWorkingDirectory;
+				std::string files = "";
+				if (Trim(msg) == "LIST")
+				{
+					std::cout << "LIST no param" << std::endl;
+				}
+				else
+				{
+					std::cout << "LIST with param" << std::endl;
+					directory = File(Trim(msg.substr(5)));
+				}
+
+				DirectoryIterator iter(directory, false, "*", File::findFilesAndDirectories);
+				while (iter.next())
+				{
+					File theFileItFound(iter.getFile());
+					if (theFileItFound.isDirectory())
+					{
+						files += "drwxr-xr-x 1 owner group\015\012";
+					}
+					else
+					{
+						files += "-rw-r--r-- 1 owner group\015\012";
+					}
+					Time lastModificationTime = theFileItFound.getLastModificationTime();
+					files += std::to_string(theFileItFound.getSize()) + " " + lastModificationTime.getMonthName(true).toStdString() + " "
+						+ std::to_string(lastModificationTime.getDayOfMonth()) + " " + std::to_string(lastModificationTime.getHours()) + ":" + std::to_string(lastModificationTime.getMinutes())
+						+ " " + theFileItFound.getFileName().toStdString() + "\n";
+				}
+
+				//tell the client that data transfer is about to happen
+				to_send = "125 Data connection already open; transfer starting.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+
+				// send
+				send(data_socket, files.c_str(), files.length(), 0);
+				std::cout << "Answered: " << files << std::endl;
+				closesocket(data_socket);
+				data_socket = INVALID_SOCKET;
+
+				// done
+				to_send = "250 Requested file action okay, completed.\n";
+
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 4) == "CDUP")
+			{
+				currentWorkingDirectory = currentWorkingDirectory.getParentDirectory();
+				currentWorkingDirectory.setAsCurrentWorkingDirectory();
+
+				to_send = "200 Okay.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 3) == "CWD")
+			{
+				currentWorkingDirectory = currentWorkingDirectory.getChildFile(Trim(msg.substr(4)));
+				currentWorkingDirectory.setAsCurrentWorkingDirectory();
+
+				to_send = "250 Okay.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 4) == "FEAT")
+			{
+				to_send = "211-Features:\nUTF8\n211 END\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+			else if (msg.substr(0, 4) == "RETR")
+			{
+				// Retrieve
+				// First, make sure you're in image mode
+				//if (!image_mode)
+				//{
+				//	// Not yet in image mode
+				//	std::cout << "Attempted to RETR before entering image mode." << std::endl;
+				//	to_send = "451 Requested action aborted: local error in processing\n";
+				//	std::cout << "Answered: " << to_send << std::endl;
+				//	send(command_socket, to_send.c_str(), to_send.size(), 0);
+				//	continue;
+				//}
+
+				// Now, make sure PORT has been called
+				if (data_socket == INVALID_SOCKET)
+				{
+					// Port hasn't been set
+					std::cout << "Attempted to RETR before porting." << std::endl;
+					to_send = "451 Requested action aborted: local error in processing\n";
+					std::cout << "Answered: " << to_send << std::endl;
+					send(command_socket, to_send.c_str(), to_send.size(), 0);
+					continue;
+				}
+
+				// get file
+				std::string fileName = msg.substr(5);
+
+				// Take out those spaces
+				fileName = Trim(fileName);
+				int sourceFile = _open(fileName.c_str(), O_RDONLY);
+
+				if (sourceFile < 0)
+				{
+					// File not found
+					std::cout << "Error opening source file: |" << msg.substr(5) << "|" << std::endl;
+					to_send = "550 Requested action not taken.\n";
+					std::cout << "Answered: " << to_send << std::endl;
+					send(command_socket, to_send.c_str(), to_send.size(), 0);
+					continue;
+				}
+
+				//tell the client that data transfer is about to happen
+				to_send = "125 Data connection already open; transfer starting.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+
+				long size = File::getCurrentWorkingDirectory().getChildFile(fileName).getSize();
+
+				//int bytes_read = 0;
+
+				//// Read and write
+				//while ((bytes_read = _read(sourceFile, recvbuf, recvbuflen)) > 0)
+				//{
+				//	// write to data port
+				//	send(data_socket, recvbuf, bytes_read, 0);
+				//	// clear buffer
+				//	recvbuf[0] = '\0';
+				//}
+
+				while (size > 0)
+				{
+					if (size > recvbuflen)
+					{
+						_read(sourceFile, recvbuf, recvbuflen);
+						// write to data port
+						send(data_socket, recvbuf, recvbuflen, 0);
+						// clear buffer
+						//recvbuf[0] = '\0';
+
+						size -= recvbuflen;
+					}
+					else
+					{
+						_read(sourceFile, recvbuf, size);
+						// write to data port
+						send(data_socket, recvbuf, size, 0);
+						// clear buffer
+						//recvbuf[0] = '\0';
+
+						size = 0;
+					}
+				}
+
+				_close(sourceFile);
+				closesocket(data_socket);
+				data_socket = INVALID_SOCKET;
+
+				// done
+				to_send = "250 Requested file action okay, completed.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+
+			}
+			else
+			{
+				// Unimplemented command
+				to_send = "502 Command not implemented.\n";
+				std::cout << "Answered: " << to_send << std::endl;
+				send(command_socket, to_send.c_str(), to_send.size(), 0);
+			}
+		}
+		else if (iResult == 0) {
+			printf("Connection closing...\n");
+		}
+		else {
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(command_socket);
+			WSACleanup();
+			return;
+		}
+
+	} while (iResult > 0);
+
+	// shutdown the connection since we're done
+	iResult = shutdown(command_socket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(command_socket);
+	}
+
+	closesocket(command_socket);
+}
+
 int __cdecl main(void)
 {
 	WSADATA wsaData;
@@ -145,9 +486,6 @@ int __cdecl main(void)
 
 	SOCKET listen_socket = INVALID_SOCKET;
 	SOCKET command_socket = INVALID_SOCKET;
-
-	char recvbuf[DEFAULT_BUFLEN];
-	int recvbuflen = DEFAULT_BUFLEN;
 
 	// Initialize Winsock
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -169,234 +507,6 @@ int __cdecl main(void)
 			continue;
 		}
 
-		std::string to_send;
-
-		bool image_mode = false;
-		SOCKET data_socket = INVALID_SOCKET;
-		File currentWorkingDirectory = File::getCurrentWorkingDirectory();
-
-		to_send = "220 Service ready for new user.\n";
-		if (send(command_socket, to_send.c_str(), to_send.length(), 0) == -1)
-		{
-			std::cout << "Error sending." << std::endl;
-		}
-
-		// Receive until the peer shuts down the connection
-		do {
-
-			iResult = recv(command_socket, recvbuf, recvbuflen, 0);
-			if (iResult > 0) {
-				std::string msg = "";
-				for (int x = 0; x < iResult; x++)
-				{
-					msg.push_back(recvbuf[x]);
-				}
-
-				std::cout << "Received: " << msg << std::endl;
-
-				if (msg.substr(0, 4) == "USER")
-				{
-					// Accept all users because I'm secure like that
-					to_send = "230 User logged in, proceed.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else if (msg.substr(0, 4) == "PASS")
-				{
-					// Accept all users because I'm secure like that
-					to_send = "230 User logged in, proceed.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else if (msg.substr(0, 4) == "NOOP")
-				{
-					// No operation
-					to_send = "200 Command okay.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else if (msg.substr(0, 4) == "QUIT")
-				{
-					// Quit
-					to_send = "221 Service closing control connection.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else if (msg.substr(0, 3) == "PWD")
-				{
-					to_send = "257 \"" + currentWorkingDirectory.getFullPathName().toStdString() + "\" is your current location\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else if (msg.substr(0, 4) == "TYPE")
-				{
-					if (msg.substr(5, 1) == "I")
-					{
-						image_mode = true;
-					}
-					else
-					{
-						image_mode = false;
-					}
-
-					to_send = "200 Command okay.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else if (msg.substr(0, 4) == "PORT")
-				{
-					// open data port
-					int c2 = msg.rfind(",");
-					int c1 = msg.rfind(",", c2 - 1);
-
-					// Calculate port number
-					int portNumber = atoi(msg.substr(c1 + 1, c2 - c1).c_str()) * 256;
-					portNumber += atoi(msg.substr(c2 + 1).c_str());
-
-					// Get address
-					std::string address = msg.substr(5, c1 - 5);
-					for (int i = 0; i < address.size(); i++)
-					{
-						if (address.substr(i, 1) == ",")
-						{
-							address.replace(i, 1, ".");
-						}
-					}
-
-					// Get the file descriptor
-					std::cout << "Attempting to open port: " << portNumber << std::endl;
-					std::cout << "From address: " << address << std::endl;
-					data_socket = getSocketFD(portNumber, false, address);
-
-					if (data_socket == INVALID_SOCKET)
-					{
-						// Port open failure
-						to_send = "425 Can't open data connection.\n";
-						std::cout << "Answered: " << to_send << std::endl;
-						send(command_socket, to_send.c_str(), to_send.size(), 0);
-						continue;
-					}
-
-					// Port open success
-					to_send = "200 Command okay.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else if (msg.substr(0, 4) == "LIST")
-				{
-					// First, make sure you're in image mode
-					if (!image_mode)
-					{
-						// Not yet in image mode
-						to_send = "451 Requested action aborted: local error in processing\n";
-						std::cout << "Answered: " << to_send << std::endl;
-						send(command_socket, to_send.c_str(), to_send.size(), 0);
-						continue;
-					}
-
-					// Now, make sure PORT has been called
-					if (data_socket == INVALID_SOCKET)
-					{
-						// Port hasn't been set
-						to_send = "451 Requested action aborted: local error in processing\n";
-						std::cout << "Answered: " << to_send << std::endl;
-						send(command_socket, to_send.c_str(), to_send.size(), 0);
-						continue;
-					}
-
-					// Open directory
-					File& directory = currentWorkingDirectory;
-					std::string files = "";
-					if (Trim(msg) == "LIST")
-					{
-						std::cout << "LIST no param" << std::endl;
-					}
-					else
-					{
-						std::cout << "LIST with param" << std::endl;
-						directory = File(Trim(msg.substr(5)));
-					}
-
-					DirectoryIterator iter(directory, false, "*", File::findFilesAndDirectories);
-					while (iter.next())
-					{
-						File theFileItFound(iter.getFile());
-						if (theFileItFound.isDirectory())
-						{
-							files += "drwxr-xr-x 1 owner group\015\012";
-						}
-						else
-						{
-							files += "-rw-r--r-- 1 owner group\015\012";
-						}
-						Time lastModificationTime = theFileItFound.getLastModificationTime();
-						files += std::to_string(theFileItFound.getSize()) + " " + lastModificationTime.getMonthName(true).toStdString() + " "
-									+ std::to_string(lastModificationTime.getDayOfMonth()) + " " + std::to_string(lastModificationTime.getHours()) + ":" + std::to_string(lastModificationTime.getMinutes())
-									+ " " + theFileItFound.getFileName().toStdString() + "\n";
-					}
-
-					//tell the client that data transfer is about to happen
-					to_send = "125 Data connection already open; transfer starting.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-
-					// send
-					send(data_socket, files.c_str(), files.length(), 0);
-					std::cout << "Answered: " << files << std::endl;
-					closesocket(data_socket);
-					data_socket = INVALID_SOCKET;
-
-					// done
-					to_send = "250 Requested file action okay, completed.\n";
-
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else if (msg.substr(0, 4) == "CDUP")
-				{
-					currentWorkingDirectory = currentWorkingDirectory.getParentDirectory();
-
-					to_send = "200 Okay.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else if (msg.substr(0, 3) == "CWD")
-				{
-					currentWorkingDirectory = currentWorkingDirectory.getChildFile(Trim(msg.substr(4)));
-					to_send = "250 Okay.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-				else
-				{
-					// Unimplemented command
-					to_send = "502 Command not implemented.\n";
-					std::cout << "Answered: " << to_send << std::endl;
-					send(command_socket, to_send.c_str(), to_send.size(), 0);
-				}
-			}
-			else if (iResult == 0) {
-				printf("Connection closing...\n");
-			}
-			else {
-				printf("recv failed with error: %d\n", WSAGetLastError());
-				closesocket(command_socket);
-				WSACleanup();
-				return 1;
-			}
-
-		} while (iResult > 0);
-
-		// shutdown the connection since we're done
-		iResult = shutdown(command_socket, SD_SEND);
-		if (iResult == SOCKET_ERROR) {
-			printf("shutdown failed with error: %d\n", WSAGetLastError());
-			closesocket(command_socket);
-			WSACleanup();
-			return 1;
-		}
-
-		// cleanup
-		closesocket(command_socket);
+		std::thread(working_thread, command_socket).detach();
 	}
 }
